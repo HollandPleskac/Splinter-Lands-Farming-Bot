31173821de333cde3f6ff31ecc821004feb1bb90
Holland Pleskac <hollandpleskac@gmail.com>
2021-01-15 23:55:14 -0800

    picking good cards and looping through the battles, fixes click error

--- farming.js
+++ farming.js
@@ -53,7 +53,49 @@ const startFarming = async (username, password) => {
   while (true) {
     await battle(page);
   }
-  
+
+}
+
+const pickCards = async (page) => {
+  await page.evaluate(() => {
+
+    let totalMana = parseInt(document.querySelector('.mana-cap').textContent.trim());
+
+    totalMana -= 10; // account for the pick of the summoner and living lava
+
+
+    const chickenCard = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta')[19].querySelector('img');
+    chickenCard.click();
+
+    const livingLavaCard = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta')[10].querySelector('img');
+    livingLavaCard.click();
+
+
+    if (totalMana < 3) {
+      return;
+    }
+
+    const serpentineSpy = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta')[2].querySelector('img');
+    serpentineSpy.click();
+    totalMana -= 3;
+
+    if (totalMana < 4) {
+      return;
+    }
+
+    const sparkPixies = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta')[11].querySelector('img');
+    sparkPixies.click();
+    totalMana -= 4;
+
+    if (totalMana < 1) {
+      return;
+    }
+
+    const creepingOoze = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta')[12].querySelector('img');
+    creepingOoze.click();
+    totalMana -= 1;
+    // when you click a card, the reference to it changes or something so the click event doesn't work if you store all of the div's in a list (get a new reference after each click works)
+  });
 }
 
 const battle = async (page) => {
@@ -86,36 +128,39 @@ const battle = async (page) => {
 
   // pick cards and battle
 
-  await page.evaluate( () => {
-    const manaCap = parseInt(document.querySelector('.mana-cap').textContent.trim());
-    const battleBtn = document.querySelector('.btn-green');
-    const card1 = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div')[0].querySelector('img');
-    card1.click();
-    battleBtn.click();
+  await pickCards(page);
+
+  await page.evaluate(() => {
+    const startBattleBtn = document.querySelector('.btn-green');
+    startBattleBtn.click();
   });
 
-  await page.waitForSelector('#btnRumble', {timeout: 250000}); // instead wait for the match to actually load
+  await page.waitForSelector('#btnRumble', { timeout: 250000 }); // instead wait for the match to actually load
   await page.waitForTimeout(10000);
-  await page.screenshot({path: './screenshots/9.png'});
+  await page.screenshot({ path: './screenshots/9.png' });
 
   // click on rumble button
   await page.click('#btnRumble');
-  await page.screenshot({path: './screenshots/10.png'});
+  await page.screenshot({ path: './screenshots/10.png' });
 
-  await page.waitForTimeout(6000); // wait for the skip button to be availiable
+  await page.waitForTimeout(8000); // wait for the skip button to be availiable
   // there is some animation to comes so wait for more time
+  // maybe wait for it to be visible instead of wating 8 seconds
 
   // click on the skip button
   await page.click('#btnSkip');
-  await page.screenshot({path: './screenshots/11.png'});
+  await page.screenshot({ path: './screenshots/11.png' });
 
   // click on close button
   await page.waitForSelector('.btn.btn--done');
   await page.waitForTimeout(2000);
   await page.click('.btn.btn--done');
-  await page.screenshot({path: './screenshots/12.png'});
+  await page.screenshot({ path: './screenshots/12.png' });
 
   await page.waitForTimeout(2000);
+
+
+
 }
 
 module.exports = startFarming;
