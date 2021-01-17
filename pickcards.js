d4f0e665caa33caa46d3df602919c30153beb5cb
Holland Pleskac <hollandpleskac@gmail.com>
2021-01-17 13:17:22 -0800

    getting the cards more dynamically, picking out a tank, then picking out archers

--- pickcards.js
+++ pickcards.js
@@ -1,16 +1,114 @@
+async function pickCards(page) {
+  await page.evaluate(() => {
+
+    function getAvailiableCards() {
+      const cards = [];
+      const cardDivs = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta');
+
+      for (i = 0; i < cardDivs.length; i++) {
+        const mana = parseInt(cardDivs[i].querySelector('.stat-mana').textContent.trim());
+        const name = cardDivs[i].querySelector('.card-name-name').textContent;
+        const isMelee = cardDivs[i].querySelector('.stat-attack');
+        const isRanged = cardDivs[i].querySelector('.stat-ranged');
+        const isMagic = cardDivs[i].querySelector('.stat-magic');
+        let attackType;
+        if (isMelee !== null) {
+          attackType = 'melee';
+        } else if (isRanged !== null) {
+          attackType = 'ranged';
+        } else if (isMagic !== null) {
+          attackType = 'magic';
+        } else {
+          attackType = 'none';
+        }
+
+        cards.push({
+          name: name,
+          mana: mana,
+          attackType: attackType,
+          position: i,
+        });
+      };
+      return cards;
+    }
+
+    function getAvailiableMana() {
+      let totalMana = parseInt(document.querySelector('.mana-cap').textContent.trim());
+      totalMana -= 3; // summoner(3)
+      return totalMana;
+    }
+
+    function getTank(cards) {
+      // check first if there is enough mana for the tank and if there is not, pick another tank
+      let highestManaCard = cards[0];
+
+      for (i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        if (card.mana > highestManaCard.mana) {
+          highestManaCard = card;
+        }
+      }
+
+      return highestManaCard;
+    };
+
+    function getArchers(totalMana, cards) {
+      let archers = [];
+      let rangedCards = cards.filter(card => card.attackType === 'ranged');
+
+      for (i = 0; i < rangedCards.length; i++) {
+        const rangedCard = rangedCards[i];
+        if (rangedCard.mana < totalMana) {
+          archers.push(rangedCard);
+        }
+        totalMana -= rangedCard.mana;
+      }
+
+      return archers;
+    }
+
+    function getCardElementByName(cardName, cards) {
+      const cardPosition = cards.filter(card => card.name === cardName)[0].position;
+      const cardElement = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta')[cardPosition].querySelector('img');
+      return cardElement;
+    }
+
+    const cards = getAvailiableCards();
+    let availiableMana = getAvailiableMana();
+    const tank = getTank(cards);
+    const archers = getArchers(availiableMana, cards);
+
+    const tankElement = getCardElementByName(tank.name, cards);
+    tankElement.click();
+    availiableMana -= tank.mana;
+
+    archers.forEach(archer => {
+      const archerElement = getCardElementByName(archer.name, cards);
+      archerElement.click();
+    });
+
+
+  });
+}
+
+
+
+
+
+
 //maybe still make a list with objects for the cards
 // calculate that position dynamically for the query selectors
 
 // so basically how you get the urls is that you do the entire url then rule_<name of your rule here>_150.png
-async function pickCards(page, type) {
-  if (type === 'https://d36mxiodymuqjm.cloudfront.net/website/icons/rulesets/new/img_combat-rule_standard_150.png') {
-    await pickStandard(page);
-  } else if (type === 'http://d36mxiodymuqjm.cloudfront.net/website/icons/rulesets/new/img_combat-rule_even-stevens_150.png') {
-    await pickEvenStevens(page);
-  } else {
-    await pickStandard(page);
-  }
-}
+// async function pickCards(page, type) {
+//   if (type === 'https://d36mxiodymuqjm.cloudfront.net/website/icons/rulesets/new/img_combat-rule_standard_150.png') {
+//     await pickStandard(page);
+//   } else if (type === 'http://d36mxiodymuqjm.cloudfront.net/website/icons/rulesets/new/img_combat-rule_even-stevens_150.png') {
+//     await pickEvenStevens(page);
+//   } else {
+//     await pickStandard(page);
+//   }
+// }
 
 
 async function pickStandard(page) {
@@ -43,7 +141,7 @@ async function pickStandard(page) {
         } else {
           attackType = 'none';
         }
-  
+
         cards.push({
           name: name,
           mana: mana,
@@ -131,7 +229,7 @@ async function pickEvenStevens(page) {
         } else {
           attackType = 'none';
         }
-  
+
         cards.push({
           name: name,
           mana: mana,
