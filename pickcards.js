f1b41da68dd0d403df52dbdf3f546dd0f36b082d
Holland Pleskac <hollandpleskac@gmail.com>
2021-01-18 11:10:08 -0800

    get the second position card

--- pickcards.js
+++ pickcards.js
@@ -18,22 +18,41 @@ async function pickCards(page) {
       const isMelee = cardDivElement.querySelector('.stat-attack');
       const isRanged = cardDivElement.querySelector('.stat-ranged');
       const isMagic = cardDivElement.querySelector('.stat-magic');
+
+      let attackType;
+      if (isMelee !== null) {
+        attackType = 'melee';
+      } else if (isRanged !== null) {
+        attackType = 'ranged';
+      } else if (isMagic !== null) {
+        attackType = 'magic';
+      } else {
+        attackType = 'none';
+      }
+
+      return attackType;
+    }
+
+    function getSecondPosAbilities(abilities) {
+      const secondPosAbilities = abilities.filter(ability => {
+        return ability === 'Opportunity' || ability === 'Reach';
+      });
+
+      return secondPosAbilities;
+    }
+
+    function getRangedAbilities(abilities) {
       const rangedAbilities = abilities.filter(ability => {
         return ability === 'Opportunity' || ability === 'Sneak';
       });
 
-      let attackType;
-        if (isMelee !== null && rangedAbilities.length === 0) {
-          attackType = 'melee';
-        } else if (isRanged !== null || rangedAbilities.length !== 0) {
-          attackType = 'ranged';
-        } else if (isMagic !== null) {
-          attackType = 'magic';
-        } else {
-          attackType = 'none';
-        }
+      return rangedAbilities;
+    }
 
-        return attackType;
+    function getAvailiableMana() {
+      let totalMana = parseInt(document.querySelector('.mana-cap').textContent.trim());
+      totalMana -= 3; // summoner 3 mana
+      return totalMana;
     }
 
     function getAvailiableCards() {
@@ -43,7 +62,7 @@ async function pickCards(page) {
       for (i = 0; i < cardDivs.length; i++) {
         const mana = parseInt(cardDivs[i].querySelector('.stat-mana').textContent.trim());
         const name = cardDivs[i].querySelector('.card-name-name').textContent;
-        
+
         const abilities = getAbilities(cardDivs[i]);
         const attackType = getAttackType(cardDivs[i], abilities);
 
@@ -51,25 +70,26 @@ async function pickCards(page) {
           name: name,
           mana: mana,
           attackType: attackType,
-          position: i,
           abilities: abilities,
+          position: i,
         });
       };
       return cards;
     }
 
-    function getAvailiableMana() {
-      let totalMana = parseInt(document.querySelector('.mana-cap').textContent.trim());
-      totalMana -= 3; // summoner(3)
-      return totalMana;
+    function getCardElementByName(cardName, cards) {
+      const cardPosition = cards.filter(card => card.name === cardName)[0].position;
+      const cardElement = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta')[cardPosition].querySelector('img');
+      return cardElement;
     }
 
     function getTank(cards) {
-      // check first if there is enough mana for the tank and if there is not, pick another tank
       let highestManaCard = cards[0];
+
 
       for (i = 0; i < cards.length; i++) {
         const card = cards[i];
+        const rangedAbilities = getSecondPosAbilities(card.abilities);
         if (card.mana > highestManaCard.mana) {
           highestManaCard = card;
         }
@@ -77,13 +97,20 @@ async function pickCards(page) {
 
       return highestManaCard;
     };
-    // get second place (opportunity or reacch)
-    // get cleanup crew (anything that fits in the mana after all archers taken)
-    // filter archers better (with opportunity etc)
 
-    function getArchers(totalMana, cards) {
+    function getSecondPositionCard(cards) {
+      let secondPositionCards = cards.filter(card => {
+        return card.attackType === 'melee' && card.abilities.includes('Opportunity') || card.abilities.includes('Reach')
+      });
+
+      return secondPositionCards[0];
+    }
+
+    function getArchers(totalMana, secondPosCard, cards) {
       let archers = [];
-      let rangedCards = cards.filter(card => card.attackType === 'ranged'); // filter for cards with abilities that let them attack in the back row
+      let rangedCards = cards.filter(card =>
+        card.attackType === 'ranged' || getRangedAbilities(card.abilities).length !== 0 && card.name !== secondPosCard.name
+      );
 
       for (i = 0; i < rangedCards.length; i++) {
         const rangedCard = rangedCards[i];
@@ -95,12 +122,6 @@ async function pickCards(page) {
       }
 
       return archers;
-    }
-
-    function getCardElementByName(cardName, cards) {
-      const cardPosition = cards.filter(card => card.name === cardName)[0].position;
-      const cardElement = document.querySelector('.deck-builder-page2__cards').querySelectorAll('div > .card.beta')[cardPosition].querySelector('img');
-      return cardElement;
     }
 
     const cards = getAvailiableCards();
@@ -118,7 +139,16 @@ async function pickCards(page) {
     tankElement.click();
     availiableMana -= tank.mana;
 
-    const archers = getArchers(availiableMana, cards);
+    try {
+      const secondPositionCard = getSecondPositionCard(cards);
+      const secondPositionElement = getCardElementByName(secondPositionCard.name, cards);
+      secondPositionElement.click();
+      availiableMana -= secondPositionCard.mana;
+    } catch (e) {
+      console.log('second position cards not availiable');
+    }
+
+    const archers = getArchers(availiableMana, secondPositionCard, cards);
     archers.forEach(archer => {
       const archerElement = getCardElementByName(archer.name, cards);
       archerElement.click();
