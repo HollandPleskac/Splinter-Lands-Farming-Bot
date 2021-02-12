7e4f498e7574e34b35d53ac4887e240cd1d16d24
Holland Pleskac <hollandpleskac@gmail.com>
2021-02-11 18:08:13 -0800

    inserting new battle log entries live

--- public/js/frontend-firebase.js
+++ public/js/frontend-firebase.js
@@ -12,7 +12,7 @@ db.collection('Battle Log').onSnapshot(querySnapshot => {
   let losses = 0;
   querySnapshot.forEach(documentSnapshot => {
     decCount += documentSnapshot.data().dec;
-    if (documentSnapshot.data().winner === 'hvcMiner') {
+    if (documentSnapshot.data().winner === 'hvcminer') {
       wins += 1;
     } else {
       losses += 1;
@@ -24,9 +24,9 @@ db.collection('Battle Log').onSnapshot(querySnapshot => {
   lossRatioElement.textContent = `L ${losses}`;
 });
 
-function insertCardImages(player, docSnap) {
-  const playerCardsDiv = document.getElementById(docSnap.id + player);
-  docSnap.data()[player + 'Team'].forEach(card => {
+function insertCardImages(player, doc) {
+  const playerCardsDiv = document.getElementById(doc.id + player);
+  doc.data()[player + 'Team'].forEach(card => {
     if (card.cardUrl === "") {
       playerCardsDiv.insertAdjacentHTML('beforeend', '<div class="empty-card"></div>');
     } else {
@@ -37,12 +37,12 @@ function insertCardImages(player, docSnap) {
   });
 }
 
-function setColorClasses(docSnap) {
-  const hvcminerName = document.getElementById(docSnap.id + 'hvcminer').previousElementSibling;
-  const opponentName = document.getElementById(docSnap.id + 'opponent').previousElementSibling;
-  const vsElement = document.getElementById(docSnap.id).querySelector('.vs').firstElementChild;
-
-  if (docSnap.winner === 'hvcMiner') {
+function setColorClasses(doc) {
+  const hvcminerName = document.getElementById(doc.id + 'hvcminer').previousElementSibling;
+  const opponentName = document.getElementById(doc.id + 'opponent').previousElementSibling;
+  const vsElement = document.getElementById(doc.id).querySelector('.vs').firstElementChild;
+  console.log(doc.data().winner);
+  if (doc.data().winner === 'hvcminer') {
     vsElement.classList.add('winner');
     vsElement.style.borderColor = "#b5ff88";
     hvcminerName.classList.add('winner');
@@ -53,31 +53,62 @@ function setColorClasses(docSnap) {
     hvcminerName.classList.add('loser');
     opponentName.classList.add('winner');
   }
-  
+
 }
 
-db.collection('Battle Log').orderBy('timestamp', 'desc').limit(40).onSnapshot(querySnapshot => {
-  querySnapshot.forEach(documentSnapshot => {
+// listen for new documents
 
-    previousBattlesElement.insertAdjacentHTML('beforeend', `
-      <div class="battle-result" id="${documentSnapshot.id}">
+db.collection('Battle Log').orderBy('timestamp', 'desc').onSnapshot(querySnapshot => {
+  querySnapshot.docChanges().forEach(change => {
+    previousBattlesElement.insertAdjacentHTML('afterbegin', `
+      <div class="battle-result" id="${change.doc.id}">
         <div>
          <div>HVCMiner</div>
-          <div class="cards-used" id="${documentSnapshot.id}hvcminer">
+          <div class="cards-used" id="${change.doc.id}hvcminer">
             
           </div>
         </div>
        <div class="vs"><p>VS</p></div>
        <div class="opponent">
-          <div>${documentSnapshot.data().opponent}</div>
-          <div class="cards-used" id="${documentSnapshot.id}opponent">
+          <div>${change.doc.data().opponent}</div>
+          <div class="cards-used" id="${change.doc.id}opponent">
 
           </div>
         </div>
       </div>
      `);
-    insertCardImages('hvcminer', documentSnapshot);
-    insertCardImages('opponent', documentSnapshot);
-    setColorClasses( documentSnapshot);
+    insertCardImages('hvcminer', change.doc);
+    insertCardImages('opponent', change.doc);
+    setColorClasses(change.doc);
+  });
+});
+
+
+
+// set the initial data
+
+
+db.collection('Battle Log').orderBy('timestamp', 'desc').limit(40).get(querySnapshot => {
+  querySnapshot.forEach(docSnap => {
+    previousBattlesElement.insertAdjacentHTML('beforeend', `
+      <div class="battle-result" id="${docSnap.id}">
+        <div>
+         <div>HVCMiner</div>
+          <div class="cards-used" id="${docSnap.id}hvcminer">
+            
+          </div>
+        </div>
+       <div class="vs"><p>VS</p></div>
+       <div class="opponent">
+          <div>${docSnap.data().opponent}</div>
+          <div class="cards-used" id="${docSnap.id}opponent">
+
+          </div>
+        </div>
+      </div>
+     `);
+    insertCardImages('hvcminer', docSnap);
+    insertCardImages('opponent', docSnap);
+    setColorClasses(docSnap);
   });
 });
