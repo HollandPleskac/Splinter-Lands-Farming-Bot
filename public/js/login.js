81103042729f112befbe93d42fc316c1d356cdf2
Holland Pleskac <hollandpleskac@gmail.com>
2021-03-19 15:20:20 -0700

    sign in

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const auth = firebase.auth();
+
+const emailTextFieldEl = document.getElementById('email-login');
+const passwordTextFieldEl = document.getElementById('password-login');
+const loginBtnEl = document.querySelector('.login-btn');
+const errorTextEl = document.querySelector('.feedback');
+
+loginBtnEl.addEventListener('click', async () => {
+  const email = emailTextFieldEl.value;
+  const password = passwordTextFieldEl.value;
+  const user = await firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(userCredential => userCredential.user)
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      errorTextEl.textContent = errorMessage;
+    });
+
+  if (user === undefined) {
+    return;
+  }
+
+  console.log('signed in');
+});
