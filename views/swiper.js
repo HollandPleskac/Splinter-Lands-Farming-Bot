a3f97392691cbd96bae94ba19fa0ac61e85667ba
Holland Pleskac <hollandpleskac@gmail.com>
2021-03-24 14:49:15 -0700

    swiper js how it works section

---
+++ views/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: '3',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
